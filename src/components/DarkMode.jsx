9651e37914a5fc75f7cc8191c555eb9a4cbfc89b
Oluwakemi <oluwakemie05@gmail.com>
2024-01-05 14:30:30 +0100

    first cmmit

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import darkPng from "../../src/assets/website/dark-mode-button.png";
+import lightPng from "../../src/assets/website/light-mode-button.png";
+const DarkMode = () => {
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+  React.useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+  const changeTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  return (
+    <div className="relative">
+      <img
+        src={darkPng}
+        alt=""
+        onClick={changeTheme}
+        className={`w-12 absolute -top-0 cursor-pointer drop-shadow-[1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 ${theme === "dark" ? "opacity-0" : "opacity-100"}` }
+      />
+      <img
+        src={lightPng}
+        onClick={changeTheme}
+        alt=""
+        className="w-12   cursor-pointer drop-shadow-[1px_1px_rgba(0,0,0,0.1)] transition-all duration-300"
+      />
+    </div>
+  );
+};
+
+export default DarkMode;
---
+++ src/components/Services.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import img2 from "../../src/assets/biryani2.png";
+
+const ServicesData = [
+  {
+    id: 1,
+    img: img2,
+    name: "Biryani",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Na ullam libero maxime rem eveniet vel ab, cupiditate quasi delectus",
+  },
+  {
+    id: 2,
+    img: img2,
+    name: "Chicken kari",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Na ullam libero maxime rem eveniet vel ab, cupiditate quasi delectus",
+  },
+  {
+    id: 2,
+    img: img2,
+    name: "Cold Coffee",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Na ullam libero maxime rem eveniet vel ab, cupiditate quasi delectus",
+  },
+];
+
+const Services = () => {
+  return (
+    <>
+      <div className="py-10">
+        <div className="container ">
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+              Our Services
+            </p>
+            <h1 className="text-3xl font-bold"> Services</h1>
+            <p className="text-xs text-gray-400">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+              ullam libero maxime rem eveniet vel ab, cupiditate quasi delectus
+              atque tempora doloremque? Quam exercitatione
+            </p>
+          </div>
+          {/* card section */}
+          <div className="grid grid-cols-1  sm:grid-cols-3  md:grid-cols-3 gap-14 md:gap-5 place-items-center">
+            {ServicesData.map(({ id, img, name, description }) => {
+              return (
+                <div key={id} className="max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 hover:bg-primary hover:text-white duration-300 p-4">
+                  <div className="h-[100px] ">
+                    <img src={img} alt="" className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300 "/>
+                  </div>
+                <div>
+                    <h1 className="text-xl font-bold">{name}</h1>
+                    <p className="text-gray-500 text-sm line-clamp-2">{description}</p>
+                </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Services;
